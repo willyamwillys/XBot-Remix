e5e165ff00d436657594fab2cb22d808615d71cd
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-08 19:29:36 +0800

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔥 XBOT-REMIX V{BOT_VER} [SUCCESSFUL ACTIVATED!]\m")
+    f"🔥 XBOT-REMIX V{BOT_VER} [SUCCESSFUL ACTIVATED!]\n"
     f"👤 [WillyamWillys](http://t.me/willyamwillys)")
 
 if len(argv) not in (1, 3, 4):
