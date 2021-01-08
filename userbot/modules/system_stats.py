576baebbcde95da732695339d8e09fffe7709128
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-08 21:36:38 +0800

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -229,14 +229,15 @@ async def pipcheck(pip):
 
 @register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
 async def amireallyalive(alive):
+    name0 = str(name.first_name)
     user = await bot.get_me()
     uptime = await get_readable_time((time.time() - StartTime))
     output = (
         f"۝⩵꙰ཱི►XBOT-REMIX◄⩵꙰ཱི۝\n running on 🤖 `{UPSTREAM_REPO_BRANCH}` 🤖\n"
         f"╭━━━━━━━━━━━━━━━━━━━━━╮\n"
-        f"┣[•👤 `USER     :`{DEFAULTUSER}\n"
+        f"┣[•👤 `USER     :`{name0}\n"
         f"┣[ 👁‍🗨 `Username :`@{user.username}\n"
-        "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
+        "`┣▰▱▰▱▰▱▰▱▰▱▰▱▰▱▰▱`\n"
         f"┣[•⚙️ `Telethon :`v {version.__version__} 🔥\n"
         f"┣[•🐍 `Python   :`v {python_version()} 🔥\n"
         f"┣[•💻 `Base on  :`{UPSTREAM_REPO_BRANCH}🔥\n"
@@ -244,7 +245,7 @@ async def amireallyalive(alive):
         f"┣[•🗃 `Modules  :`{len(modules)} Loaded🔥\n"
         f"┣[•🕒 `Uptime   :`{uptime} 🔥\n"
         f"╰━━━━━━━━━━━━━━━━━━━━━╯\n"
-        f"      •MOD BY : `{DEFAULTUSER}`")
+        f"┣-•MOD BY : `{DEFAULTUSER}`")
     if ALIVE_LOGO:
         try:
             logo = ALIVE_LOGO
