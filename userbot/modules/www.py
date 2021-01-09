0c67c308786b99c07ce3bc524afd0b2c85a80c5f
willyamwillys <76863131+willyamwillys@users.noreply.github.com>
2021-01-09 09:17:58 +0800

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -88,7 +88,7 @@ async def pingme(pong):
     await pong.edit("`Pinging....`")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
-    await pong.edit(f"**PONG!! 🍭**\n**Pinger** : %sms\n**Bot Uptime** : {uptime}🕛" % (duration))
+    await pong.edit(f"➡️ **PONG!! 🍭**\n➡️ **Pinger** : %sms 📶\n➡️ **Bot Uptime** : {uptime}🕛" % (duration))
 
 
 @register(outgoing=True, pattern="^.pong$")
